f5ef1c5a32e504ca171c8c7800590431a714e53f
Lotuslinny <67099000+Lotuslinny@users.noreply.github.com>
2020-09-03 11:49:25 +0200

    Add files via upload

---
+++ M03_L13_if_else_StatementI/script.js
@@ -0,0 +1,29 @@
+// Deel 1
+const age = 40;
+
+if (age > 17) {
+  console.log('you may enter');
+} else if (age < 18) {
+  console.log('please stay outside');
+}
+
+
+// Deel 2
+const isFemale = 'male';
+
+if (isFemale == "female") {
+  console.log('true');
+} else if (isFemale == "male") {
+  console.log('false');
+}
+
+
+// Deel 3
+
+const driverStatus = 'Jason';
+
+if (driverStatus == "Bob") {
+  console.log("You can drive");
+} else if (driverStatus == "Jason") {
+  console.log("You may not drive");
+}  
